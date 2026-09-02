75f0b405bc7e02f1653a7725e5452e5e684ef5f3
prachimk10220 <prachimahakal6@gmail.com>
2026-09-02 20:20:56 +0530

    Add files via upload

---
+++ PRA1.C
@@ -0,0 +1,23 @@
+/* 
+Program (1) Write a program to make use of basic Input/Output functions using different data types
+Solution (1) Predefined Values
+*/
+
+#include <stdio.h>
+int main()
+{ 
+    int rollnum;
+    float per;
+    char grade;
+
+    rollnum = 11;
+    per = 75.5;
+    grade = 'A';
+
+    printf("-----Student Information-----\n");
+    printf("\nRoll Number: %d", rollnum);
+    printf("\nPercentage: %f", per);
+    printf("\nGrade: %c", grade);
+
+    return 0;
+}
